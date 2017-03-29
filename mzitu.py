8477a3229383ebab2535c88d90c0df91d14ca6e0
xmyxiao <958389530@qq.com>
2017-03-29 21:16:58 +0800

    03-29

--- mzitu.py
+++ mzitu.py
@@ -1,44 +1,45 @@
-# -*- coding: utf-8 -*-
-
-import requests
 from bs4 import BeautifulSoup
 import os
+from Download import request ##导入模块变了一下
+from pymongo import MongoClient
 
 class mzitu():
 
+
     def all_url(self, url):
-        html = self.request(url)
+
+        html = request.get(url, 3) ##这儿更改了一下（是不是发现  self 没见了？）
         all_a = BeautifulSoup(html.text, 'lxml').find('div', class_='all').find_all('a')
         for a in all_a:
             title = a.get_text()
-            print(u'开始保存：', title) 
-            path = str(title).replace("?", '_') 
-            self.mkdir(path) 
+            print(u'开始保存：', title)
+            path = str(title).replace("?", '_')
+            self.mkdir(path)
             os.chdir("D:\mzitu\\"+path)
             href = a['href']
-            self.html(href) 
+            self.html(href)
 
-    def html(self, href):   
-        html = self.request(href)
-        max_span = BeautifulSoup(html.text, 'lxml').find('div', class_='pagenavi').find_all('span')[-2].get_text()
+    def html(self, href):
+        html = request.get(href, 3)##这儿更改了一下（是不是发现  self 没见了？）
+        max_span = BeautifulSoup(html.text, 'lxml').find_all('span')[10].get_text()
         for page in range(1, int(max_span) + 1):
             page_url = href + '/' + str(page)
-            self.img(page_url) 
+            self.img(page_url)
 
-    def img(self, page_url): 
-        img_html = self.request(page_url)
+    def img(self, page_url):
+        img_html = request.get(page_url, 3) ##这儿更改了一下（是不是发现  self 没见了？）
         img_url = BeautifulSoup(img_html.text, 'lxml').find('div', class_='main-image').find('img')['src']
         self.save(img_url)
 
-    def save(self, img_url): 
+    def save(self, img_url):
         name = img_url[-9:-4]
-        img = self.request(img_url)
+        print(u'开始保存：', img_url)
+        img = request.get(img_url, 3) ##这儿更改了一下（是不是发现  self 没见了？）
         f = open(name + '.jpg', 'ab')
-        print(name)
         f.write(img.content)
         f.close()
 
-    def mkdir(self, path):
+    def mkdir(self, path): 
         path = path.strip()
         isExists = os.path.exists(os.path.join("D:\mzitu", path))
         if not isExists:
@@ -49,10 +50,8 @@ class mzitu():
             print(u'名字叫做', path, u'的文件夹已经存在了！')
             return False
 
-    def request(self, url): 
-        headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"}
-        content = requests.get(url, headers=headers)
-        return content
 
-Mzitu = mzitu() 
-Mzitu.all_url('http://www.mzitu.com/all') 
+
+
+Mzitu = mzitu() ##实例化
+Mzitu.all_url('http://www.mzitu.com/all') ##给函数all_url传入参数  你可以当作启动爬虫（就是入口）
